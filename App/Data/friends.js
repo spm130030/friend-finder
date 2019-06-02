52204c4caae58525ab9bfdbbf4248b523f53a0fb
serena mallavarapu <serena.mallavarapu@gmail.com>
2019-06-01 21:37:16 -0500

    first

---
+++ App/Data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: "Cooper",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/d/de/Beagle_Upsy.jpg",
+		scores: [
+			"1",
+			"5",
+			"5",
+			"1",
+			"5",
+			"5",
+			"1",
+			"1",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "Tank",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/0/05/Pomeranian_Dog_Female_Three_Years_Old.jpg",
+		scores: [
+			"2",
+			"4",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"2",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "Chase",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/f/f0/Goldensondika.jpg",
+		scores: [
+			"4",
+			"3",
+			"5",
+			"1",
+			"1",
+			"5",
+			"5",
+			"1",
+			"1",
+			"1"
+		]
+	},
+	{
+		name: "Connie",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/3/3b/My_dog_back_in_Korea.png",
+		scores: [
+			"5",
+			"1",
+			"2",
+			"1",
+			"3",
+			"4",
+			"1",
+			"3",
+			"3",
+			"4"
+		]
+	},
+	{
+		name: "Thor",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/5/51/DSHwiki.jpg",
+		scores: [
+			"1",
+			"1",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"1"
+		]
+	},
+	{
+		name: "Bolt",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Pug_-_1_year_Old.jpg",
+		scores: [
+			"1",
+			"1",
+			"5",
+			"1",
+			"3",
+			"2",
+			"5",
+			"1",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "Kim K",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Kim_Kardashian_2018_5.png",
+		scores: [
+			"5",
+			"4",
+			"4",
+			"2",
+			"4",
+			"4",
+			"3",
+			"2",
+			"3",
+			"3"
+		]
+	},
+	{
+		name: "Khalid",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/d/df/Khalid-MTV_smiling.jpg",
+		scores: [
+			"5",
+			"2",
+			"3",
+			"1",
+			"5",
+			"1",
+			"3",
+			"1",
+			"1",
+			"5"
+		]
+	},
+	{
+		name: "Kermit the Frog",
+		photo: "http://i.dailymail.co.uk/i/pix/2015/09/02/08/2BE5406100000578-3218820-image-m-25_1441179775589.jpg",
+		scores: [
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
+		]
+	},
+	{
+		name: "Michelle Obama",
+		photo: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Michelle_Obama_2013_official_portrait.jpg",
+		scores: [
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1",
+			"1"
+		]
+	}
+];
+
+module.exports = friendsArray;
