38b18e2caa7cab09d1b4a4092afe24bb846fb571
serena mallavarapu <serena.mallavarapu@gmail.com>
2019-08-01 22:03:30 -0500

    foobar

--- App/Routing/htmlRoutes.js
+++ App/Routing/htmlRoutes.js
@@ -7,11 +7,11 @@ module.exports = function(app) {
 
 	// Home page
 	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/home.html"));
+		res.sendFile(path.join(__dirname, "/../public/home.html"));
 	});
 
 	// Survey page
 	app.get("/survey", function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/survey.html"));
+		res.sendFile(path.join(__dirname, "/../public/survey.html"));
 	});
 };
